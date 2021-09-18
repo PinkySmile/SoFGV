7a488f0433554531d7cf8e99e1002e93970cde29
Andgel <andgel.halley@epitech.eu>
2021-09-18 20:02:17 +0200

    Rename inputs

--- src/InputEnum.hpp
+++ src/InputEnum.hpp
@@ -12,21 +12,26 @@ namespace Battle
 		INPUT_RIGHT,
 		INPUT_UP,
 		INPUT_DOWN,
+		INPUT_N,
+		INPUT_NEUTRAL = INPUT_N,
+		INPUT_M,
+		INPUT_MATTER = INPUT_M,
+		INPUT_S,
+		INPUT_SPIRIT = INPUT_S,
+		INPUT_V,
+		INPUT_VOID = INPUT_V,
 		INPUT_A,
-		INPUT_B,
-		INPUT_C,
-		INPUT_D,
-		INPUT_E,
+		INPUT_ASCEND = INPUT_A,
 	};
 
 	struct Input {
 		int horizontalAxis;
 		int verticalAxis;
+		int n;
+		int m;
+		int s;
+		int v;
 		int a;
-		int b;
-		int c;
-		int d;
-		int e;
 	};
 }
 
