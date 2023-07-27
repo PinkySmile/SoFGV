5293cdaec7e234945016c19fc79bdf5161dc12aa
yolotemperance <benoitpineau02@gmail.com>
2023-07-27 15:13:40 +0200

    added types page

--- src/WikiGenerator/generate_all.py
+++ src/WikiGenerator/generate_all.py
@@ -10,7 +10,7 @@ for f in os.listdir("assets/characters"):
 	if f == "template":
 		continue
 	try:
-		d = generate_wiki_page("assets/characters/" + f, no_regen=len(sys.argv) >= 1)
+		d = generate_wiki_page("assets/characters/" + f, no_regen=len(sys.argv) >= 2)
 		entries.append(d)
 	except:
 		traceback.print_exc()
