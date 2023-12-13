4daae4325ca0e7c4fb69fbcbf9f74b9749d6c9a5
PinkySmile <Gegel85@laposte.net>
2023-12-13 23:42:08 +0100

    Bumped version number

--- src/LibCore/Resources/version.h
+++ src/LibCore/Resources/version.h
@@ -5,7 +5,7 @@
 #ifndef SOFGV_VERSION_H
 #define SOFGV_VERSION_H
 
-#define REAL_VERSION_STR "alpha 0.4.3"
+#define REAL_VERSION_STR "alpha 0.4.4"
 #ifdef _DEBUG
 #define VERSION_STR REAL_VERSION_STR" DEBUG"
 #else
