177f066fec1e6e79b5187e985d1b86c52e57c2cf
PinkySmile <Gegel85@laposte.net>
2024-12-14 15:40:23 +0100

    Removed test menu entry

--- src/Game/Scenes/MainMenu/TitleScreen.cpp
+++ src/Game/Scenes/MainMenu/TitleScreen.cpp
@@ -103,7 +103,6 @@ namespace SpiralOfFate
 	TitleScreen::TitleScreen() :
 		_menuObject{"assets/ui/copperplate-gothic-light.ttf", "assets/ui/gillsansmt.ttf", {
 			{
-				{"test", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", nullptr},
 				{"Solo mode", "Fight by yourself", [this]{
 					this->_menuObject.setEnabledMenu(1, false);
 				}},
@@ -116,6 +115,9 @@ namespace SpiralOfFate
 				{"Extra", "Other", [this]{
 					this->_menuObject.setEnabledMenu(4, false);
 				}},
+			#ifdef _DEBUG
+				{"Debug", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", nullptr},
+			#endif
 				{"Quit", "Quit game", []{
 					game->screen->close();
 				}},
